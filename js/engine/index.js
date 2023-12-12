45e04440cc8a2250a160959ab3192d3b93f6f728
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2023-12-12 23:28:03 +0000

    starting again from scratch...

--- js/engine/index.js
+++
@@ -1,8 +0,0 @@
-import img from "./img.js";
-const canvas = document.createElement("canvas");
-document.body.append(canvas);
-const ctx = canvas.getContext("2d");
-canvas.width = 2000/1.5;
-canvas.height = 1143/1.5;
-
-const background = img("bg.png");
